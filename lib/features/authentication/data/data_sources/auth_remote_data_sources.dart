6d5a07d1e44ac88fa40b7750a253e8bfe5bb9324
Yihun <yankure01@gmail.com>
2024-02-27 19:21:17 +0300

    fdb-09

--- lib/features/authentication/data/data_sources/auth_remote_data_sources.dart
+++ lib/features/authentication/data/data_sources/auth_remote_data_sources.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
+import 'package:authentication_app/core/errors/failure.dart';
 import 'package:authentication_app/core/utils/constant.dart';
 import 'package:authentication_app/features/authentication/data/models/user_model.dart';
-import 'package:http/http.dart'as http;
+import 'package:http/http.dart' as http;
 
 // final http = Client();
 
@@ -16,22 +19,55 @@ abstract class AuthenticationRemoteDataSource {
 
 class AuthenticationRemoteDataSourceImpl
     implements AuthenticationRemoteDataSource {
-  
   final http.Client _client;
 
-  AuthenticationRemoteDataSourceImpl({required http.Client client}) : _client = client;
+  AuthenticationRemoteDataSourceImpl({required http.Client client})
+      : _client = client;
 
   @override
   Future<void> createUser(
       {required String createdAt,
       required String name,
       required String avatar}) async {
-    final response = await _client.post(Uri.parse('$kBaseUrl/users'));
+    try {
+      final response = await _client.post(
+        Uri.parse('$kBaseUrl/users'),
+        body: jsonEncode(
+          {
+            'createdAt': createdAt,
+            'name': name,
+            'avatar': avatar,
+          },
+        ),
+      );
+      if (response.statusCode != 200 && response.statusCode != 201) {
+        throw ApiFailure(
+            message: response.body, statusCode: response.statusCode);
+      }
+    } on ApiFailure {
+      rethrow;
+    } catch (e) {
+      throw ApiFailure(message: e.toString(), statusCode: 505);
+    }
   }
 
   @override
   Future<List<UserModel>> getUsers() async {
-    // TODO: implement getUsers
-    throw UnimplementedError();
+    try {
+      final response = await _client.get(Uri.parse('$kBaseUrl/users'));
+      final responseResult =
+          List<Map<String, dynamic>>.from(jsonDecode(response.body))
+              .map((userData) => UserModel.fromMap(userData))
+              .toList();
+      if (response.statusCode != 200) {
+        throw ApiFailure(
+            message: response.body, statusCode: response.statusCode);
+      }
+      return responseResult;
+    } on ApiFailure {
+      rethrow;
+    } catch (e) {
+      throw ApiFailure(message: e.toString(), statusCode: 505);
+    }
   }
 }
